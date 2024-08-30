0d81c9592cc30750d5fcc82bbd5e50d8be6bd1b1
Jocelyn <ext.jrazafimaharo@enerlis.fr>
2024-08-30 17:41:15 +0300

    add motion

--- assets/vue/main.ts
+++ assets/vue/main.ts
@@ -5,6 +5,7 @@ import router from './router'
 import { createPinia } from 'pinia'
 import 'material-icons/iconfont/material-icons.css'; // https://www.npmjs.com/package/material-icons
 import 'material-symbols'; // https://www.npmjs.com/package/material-symbols
+import { MotionPlugin } from '@vueuse/motion' // https://motion.vueuse.org/
 
 const app = createApp(App)
 
@@ -12,4 +13,5 @@ import '../css/index.css'
 
 app.use(router)
    .use(createPinia())
+   .use(MotionPlugin)
    .mount('#app')
--- assets/vue/views/home/HomeView.vue
+++ assets/vue/views/home/HomeView.vue
@@ -5,22 +5,39 @@
         <div class="flex flex-col justify-center items-center gap-4">
             <Button>Click me</Button>
             <div>
-            <Select>
-                <SelectTrigger class="w-[180px]">
-                    <SelectValue placeholder="Select a fruit" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectGroup>
-                        <SelectLabel>Fruits</SelectLabel>
-                        <SelectItem v-for="(item, index) in items" 
-                            :key="index"
-                            :value="item.value">
-                            {{ item.label }}
-                        </SelectItem>
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
+                <Select>
+                    <SelectTrigger class="w-[180px]">
+                        <SelectValue placeholder="Select a fruit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Fruits</SelectLabel>
+                            <SelectItem v-for="(item, index) in items" 
+                                :key="index"
+                                :value="item.value">
+                                {{ item.label }}
+                            </SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
         </div>
+        <div
+            v-motion
+            :initial="{ opacity: 0, y: 100 }"
+            :enter="{ opacity: 1, y: 0, scale: 1, }"
+            :variants="{ custom: { scale: 2 } }"
+            :hovered="{ y: 100 , transition: {
+                            type: 'spring',
+                            stiffness: 250,
+                            damping: 25,
+                            mass: 0.5,
+                        },
+                      }"
+            :delay="200"
+            :duration="1200"
+            class="text-center mt-5">
+            <h1>Title</h1>
         </div>
     </div>
 </template>
